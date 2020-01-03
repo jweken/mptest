23cd27bcf139c7221f84632fe4aaa0269ce943c7
jweken <jweken@kpnmail.nl>
2020-01-03 08:07:03 +0100

    update all

--- bin/mp.cmd
+++ bin/mp.cmd
@@ -4,7 +4,7 @@ echo.
 echo make project %1
 
 if "%1"=="" goto message
-md C:\dev\%1 && cd C:\dev\%1 && echo # %1 > README.md && cat c:\dev\licenses\MIT-LICENSE.txt > LICENCE && code .
+md C:\dev\%1 && cd C:\dev\%1 && echo # %1 > ReadMe.md && cat c:\dev\licenses\MIT-LICENSE.txt > LICENCE && code .
 :message
 echo I need a projectname
 goto end
